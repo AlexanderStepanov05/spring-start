9d33fb6958f50fd61b14af668bd3f3a2c9ced7da
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2024-03-15 11:59:57 +0300

    xml_application_context

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,12 @@
 package org.example;
 
-import org.example.database.ioc.Container;
+
 import org.example.database.pool.ConnectionPool;
-import org.example.database.repository.CompanyRepository;
-import org.example.database.repository.UserRepository;
-import org.example.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        var container = new Container();
-
-//        var connectionPool = new ConnectionPool();
-        var connectionPool = container.get(ConnectionPool.class);
-        var userRepository = container.get(UserRepository.class);
-        var companyRepository = container.get(CompanyRepository.class);
-        var userService = container.get(UserService.class);
-
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        System.out.println(context.getBean("pool1", ConnectionPool.class));
     }
 }
