41b43aaf8cd8877bb00c23f88276ef305200d8db
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2024-03-17 22:35:40 +0300

    bpp

--- src/main/java/org/example/database/repository/CompanyRepository.java
+++ src/main/java/org/example/database/repository/CompanyRepository.java
@@ -1,16 +1,32 @@
 package org.example.database.repository;
 
+import jakarta.annotation.PostConstruct;
+import org.example.bpp.InjectBean;
+import org.example.bpp.Transaction;
+import org.example.database.entity.Company;
 import org.example.database.pool.ConnectionPool;
 
-public class CompanyRepository {
+import java.util.Optional;
 
-    private final ConnectionPool connectionPool;
+@Transaction
+public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
+    @InjectBean
+    private ConnectionPool connectionPool;
+
+    @PostConstruct
+    private void init() {
+        System.out.println("Init CompanyRepository...");
     }
 
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
+    @Override
+    public Optional<Company> findById(Integer id) {
+        System.out.println("Find by id method...");
+        return Optional.of(new Company(id));
+    }
+
+    @Override
+    public void delete(Company entity) {
+        System.out.println("Delete method...");
     }
 }
